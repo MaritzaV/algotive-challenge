0e834c0a7226c97b753abae52cffdc197fc6ccd6
Maritza Evian Valdez <maritzavaldez@mevaldez-mac.local>
2024-02-18 12:00:23 -0600

    Adds Component VideoTable

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import VideoTable from './components/VideoTable'
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div> 
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div> 
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <>
+      <div className="flex flex-row">
+        <nav className="basis-4/12">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Video Gallery</a></li>
+                <li><a href="#">Contacto</a></li>
+            </ul>
+        </nav>
+        <div className='basis-8/12'>
+         <VideoTable />
+        </div>
     </div>
+    </>
   )
 }
 
